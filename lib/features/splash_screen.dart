0f902c09ccc4a31adb6cf10a561a6ef950a83d3a
luis <valdivialuis1989@gmail.com>
2020-11-14 14:40:50 -0500

    splash

--- lib/features/splash_screen.dart
+++ lib/features/splash_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:primer_chapoteo/global/custom_color.dart';
 
 class SplashScreen extends StatefulWidget {
   @override
@@ -11,8 +13,46 @@ class _SplashScreenState extends State<SplashScreen> {
     return Scaffold(
         body: SafeArea(
       child: Container(
-        child: Row(
-          children: [Text("Hola"), Text("Hola"), Text("Hola"), Text("Hola")],
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Stack(
+          children: [
+            Container(
+              alignment: Alignment.center,
+              child: Image(
+                  width: 200, image: AssetImage("assets/images/logo.png")),
+            ),
+            Positioned(
+              child: Column(
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      print("HOLA");
+                    },
+                    child: Container(
+                      width: 45,
+                      margin: EdgeInsets.only(bottom: 10),
+                      height: 45,
+                      child:
+                          Icon(Icons.keyboard_arrow_right, color: Colors.white),
+                      decoration: BoxDecoration(
+                          shape: BoxShape.circle, color: CustomColor.mainColor),
+                    ),
+                  ),
+//                  SizedBox(height: 20,),
+                  Text(
+                    "GO",
+                    style: TextStyle(
+                      color: CustomColor.mainColor,
+                      fontSize: 18,
+                    ),
+                  )
+                ],
+              ),
+              right: 60,
+              bottom: 50,
+            ),
+          ],
         ),
       ),
     ));
---
+++ lib/global/custom_color.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+
+class CustomColor{
+  static Color mainColor = HexColor("#000A3A");
+}
